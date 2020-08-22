898593e59f84eb1fb78e7ff80788ab1840e58bf7
Dat Hoang <htdat@users.noreply.github.com>
2020-08-22 19:31:44 +0700

    try to dispaly something

--- src/App.js
+++ src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {
+  SingleDatePicker
+} from "react-google-flight-datepicker";
+import "react-google-flight-datepicker/dist/main.css";
+
 
 function App() {
+
+  const [date, setDate] = useState( new Date()); 
+
+  const onChange = (newDate) => {
+    console.log( );
+    setDate(newDate); 
+  }
   return (
-    <div>React calendar</div>
-  );
+    <>
+      <SingleDatePicker startDate={date} onChange={onChange}/>
+      Selected Date: {date.toString()};
+    </>  
+
+    );
 }
 
 export default App;
